eab15d72de2b78da4dd7195dca3a93667522f176
Phantasmic <elieblanchet.ca@gmail.com>
2025-10-25 20:32:02 -0400

    ObscurioniteLeggings: use float move/ranged bonuses and apply them

--- Content/Items/Armor/Obscurionite/ObscurioniteLeggings.cs
+++ Content/Items/Armor/Obscurionite/ObscurioniteLeggings.cs
@@ -4,27 +4,29 @@ using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
 
-namespace SpiritrumReborn.Content.Items.Armor
+namespace SpiritrumReborn.Content.Items.Armor.Obscurionite
 {
 	// The AutoloadEquip attribute automatically attaches an equip texture to this item.
 	// Providing the EquipType.Legs value here will result in TML expecting a X_Legs.png file to be placed next to the item's main texture.
 	[AutoloadEquip(EquipType.Legs)]
 	public class ObscurioniteLeggings : ModItem
 	{
-		public static readonly int MoveSpeedBonus = 5;
+		public static readonly float MoveSpeedBonus = 0.20f; 
+		public static readonly float RangedDamageBonus = 0.15f;
 
-		public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(MoveSpeedBonus);
+		public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs((int)(MoveSpeedBonus * 100));
 
 		public override void SetDefaults() {
 			Item.width = 18; // Width of the item
 			Item.height = 18; // Height of the item
 			Item.value = Item.sellPrice(gold: 1); // How many coins the item is worth
 			Item.rare = ItemRarityID.Green; // The rarity of the item
-			Item.defense = 5; // The amount of defense the item will give when equipped
+			Item.defense = 9; // The amount of defense the item will give when equipped
 		}
 
 		public override void UpdateEquip(Player player) {
-			player.moveSpeed += MoveSpeedBonus / 100f; // Increase the movement speed of the player
+			player.moveSpeed += MoveSpeedBonus; // Increase the movement speed of the player
+			player.GetDamage(DamageClass.Ranged) += RangedDamageBonus;
 		}
 
 		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
