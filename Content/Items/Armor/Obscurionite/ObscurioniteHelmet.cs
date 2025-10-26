00653f24e17a656e6f36d7da30fba2d18a50121d
Phantasmic <elieblanchet.ca@gmail.com>
2025-10-25 20:28:38 -0400

    ObscurioniteHelmet: add melee bonuses and set crit chance

--- Content/Items/Armor/Obscurionite/ObscurioniteHelmet.cs
+++ Content/Items/Armor/Obscurionite/ObscurioniteHelmet.cs
@@ -5,14 +5,17 @@ using Terraria.ModLoader;
 using Terraria.GameContent.Creative;
 using SpiritrumReborn.Content.Items.Materials;
 
-namespace SpiritrumReborn.Content.Items.Armor
+namespace SpiritrumReborn.Content.Items.Armor.Obscurionite
 {
 	// The AutoloadEquip attribute automatically attaches an equip texture to this item.
 	// Providing the EquipType.Head value here will result in TML expecting a X_Head.png file to be placed next to the item's main texture.
 	[AutoloadEquip(EquipType.Head)]
 	public class ObscurioniteHelmet : ModItem
 	{
-		public static readonly int AdditiveGenericDamageBonus = 20;
+		public const int MeleeSpeedBonus = 15; 
+		public const int MeleeDamageBonus = 10;  
+
+		public const int SetBonusCritChance = 15; 
 
 		public static LocalizedText SetBonusText { get; private set; }
 
@@ -23,7 +26,7 @@ namespace SpiritrumReborn.Content.Items.Armor
 			// ArmorIDs.Head.Sets.DrawFullHair[Item.headSlot] = true; // Draw all hair as normal. Used by Mime Mask, Sunglasses
 			// ArmorIDs.Head.Sets.DrawsBackHairWithoutHeadgear[Item.headSlot] = true;
 
-			SetBonusText = this.GetLocalization("SetBonus").WithFormatArgs(AdditiveGenericDamageBonus);
+			SetBonusText = this.GetLocalization("SetBonus").WithFormatArgs(SetBonusCritChance);
 		}
 
 		public override void SetDefaults() {
@@ -31,7 +34,13 @@ namespace SpiritrumReborn.Content.Items.Armor
 			Item.height = 18; // Height of the item
 			Item.value = Item.sellPrice(gold: 1); // How many coins the item is worth
 			Item.rare = ItemRarityID.Green; // The rarity of the item
-			Item.defense = 5; // The amount of defense the item will give when equipped
+			Item.defense = 9; // The amount of defense the item will give when equipped
+		}
+
+		public override void UpdateEquip(Player player)
+		{
+			player.GetAttackSpeed(DamageClass.Melee) += MeleeSpeedBonus / 100f;
+			player.GetDamage(DamageClass.Melee) += MeleeDamageBonus / 100f;
 		}
 
 		// IsArmorSet determines what armor pieces are needed for the setbonus to take effect
@@ -42,7 +51,7 @@ namespace SpiritrumReborn.Content.Items.Armor
 		// UpdateArmorSet allows you to give set bonuses to the armor.
 		public override void UpdateArmorSet(Player player) {
 			player.setBonus = SetBonusText.Value; // This is the setbonus tooltip: "Increases dealt damage by 20%"
-			player.GetDamage(DamageClass.Generic) += AdditiveGenericDamageBonus / 100f; // Increase dealt damage for all weapon classes by 20%
+			player.GetCritChance(DamageClass.Generic) += SetBonusCritChance;
 		}
 
 		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
